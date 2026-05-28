cd77d4b3129acfedd665895fa6f126db453b0b69
Ren Stanforth <mail@renstanforth.com>
2026-05-29 02:25:35 +0800

    sessions, errors & flash messages

--- task-list/resources/views/layouts/app.blade.php
+++ task-list/resources/views/layouts/app.blade.php
@@ -2,10 +2,16 @@
 <html lang="en">
 <head>
     <title>Laravel 10 Task List App</title>
+    @yield('styles')
 </head>
 <body>
     <h1>@yield('title')</h1>
     <div>
+        @if(session()->has('success'))
+            <div class="success-message">
+                {{ session('success') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
