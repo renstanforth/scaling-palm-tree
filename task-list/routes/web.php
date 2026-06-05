7ba8aa85d0da25785f48a4a775b897eddfa62cb3
Ren Stanforth <mail@renstanforth.com>
2026-06-06 04:22:05 +0800

    deleting data

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -44,6 +44,13 @@ Route::put('/tasks/{task}', function(TaskRequest $request, Task $task){
   ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
 
+Route::delete('/tasks/{task}', function(Task $task){
+  $task->delete();
+
+  return redirect()->route('tasks.index')
+  ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
+
 // Route::get('/hello', function(){
 //     return 'Hello';
 // })->name('hello');
